a432abc0a74a984d2e0ad9ff0ad1ab3dc6d2d2fb
Temirlan <Timb1594@gmail.com>
2025-07-07 09:53:47 +0600

    Search

--- src/main/java/com/autobots/_java/lambda/weekdays/Monday.java
+++ src/main/java/com/autobots/_java/lambda/weekdays/Monday.java
@@ -1,4 +1,18 @@
 package com.autobots._java.lambda.weekdays;
 
 public class Monday {
+    public static void main(String[] args) {
+        Monday m = new Monday();
+        System.out.println(m.roundIfNeeded(17));
+        System.out.println(m.roundIfNeeded(12));
+    }
+
+    public int roundIfNeeded(int num) {
+        int lastDigit = num % 10;
+        if (lastDigit >= 5) {
+            return (num / 10 + 1) * 10; // округлить вверх до ближайшего десятка
+        } else {
+            return (num / 10) * 10;     // округлить вниз
+        }
+    }
 }
